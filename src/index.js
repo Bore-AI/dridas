ee5984c8f290d79baf55d820e31d2e07333f268d
Fredrik Bore <fredrik@bore.ai>
2022-08-23 21:53:35 +0200

    bump packages

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "normalize.css";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
